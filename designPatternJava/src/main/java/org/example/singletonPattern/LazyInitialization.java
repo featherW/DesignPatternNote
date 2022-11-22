a77d6c0163b7f7bcf17446aac4f46142c64687a0
featherW <wangyu@tuya.com>
2022-11-22 11:58:56 +0800

    [ADD] 懒汉模式

---
+++ designPatternJava/src/main/java/org/example/singletonPattern/LazyInitialization.java
@@ -0,0 +1,15 @@
+package org.example.singletonPattern;
+
+public class LazyInitialization {
+    private static LazyInitialization instance;
+
+    private LazyInitialization() {
+    }
+
+    public static LazyInitialization getInstance() {
+        if (instance == null) {
+            instance = new LazyInitialization();
+        }
+        return instance;
+    }
+}
--- design_pattern_python/singleton_pattern/lazy_initialization.py
+++ design_pattern_python/singleton_pattern/lazy_initialization.py
@@ -2,16 +2,17 @@ class EagerInitialization(object):
     __instance = None
 
     def __new__(cls, *args, **kwargs):
+        raise Exception("禁用new")
+
+    @staticmethod
+    def getinstance():
         if EagerInitialization.__instance is None:
             EagerInitialization.__instance = object.__new__(EagerInitialization)
         return EagerInitialization.__instance
 
-    def hello(self):
-        print("hello")
-
 
 if __name__ == '__main__':
-    e1 = EagerInitialization()
-    e2 = EagerInitialization()
+    e1 = EagerInitialization.getinstance()
+    e2 = EagerInitialization.getinstance()
     print("e1->addr: {}\ne2->addr: {}".format(e1, e2))
     print(e1 == e2)
